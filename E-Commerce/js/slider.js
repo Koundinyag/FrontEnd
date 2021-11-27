471257e0816eb9e18b626cac67604f5b6504ffa5
Koundu-G <84827185+Koundinyag@users.noreply.github.com>
2021-11-27 23:43:59 +0530

    Delete E-Commerce directory

--- E-Commerce/js/slider.js
+++
@@ -1,9 +0,0 @@
-const slider = document.getElementById('glide')
-
-new Glide(slider, {
-    type: 'carousel',
-    startAt: 0,
-    preView: 1,
-    animationDuration: 800,
-    animationTimingFunc:"linear",
-}).mount();
